7e6f8a64b566b9ee5fb3706da9bdbcc2bdebfc4d
MSKalin <94841878+MSKalin@users.noreply.github.com>
2021-11-26 20:44:10 +0300

    Add files via upload

---
+++ vid-and-music.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>msmaksite</title>
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+<header>
+<nav class="navbar navbar-light bg-light fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">MSMAKSITE</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasNavbarLabel"></h5>
+        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="/">Главная</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="">Музыка и видео</a></li>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="/">Сайты</a></li>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</nav>
+</header>
+<br><br><br>
+<div class="card-group">
+<div class="card" style="width: 19rem;">
+  <img src="img/pss.png" class="card-img-top">
+  <div class="card-body">
+    <h5 class="card-title">MDK - Press Start (SDV by MS)</h5>
+    <a href="music/pss.php" class="btn btn-primary">Слушать</a>
+  </div>
+</div> <div class="card" style="width: 19rem;">
+  <img src="img/fb.png" class="card-img-top">
+  <div class="card-body">
+    <h5 class="card-title">MDK - Fingerbang (SDV by MS)</h5>
+    <a href="music/fb.php" class="btn btn-primary">Слушать</a>
+  </div>
+</div>
+</div>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
